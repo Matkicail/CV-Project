ad733f2dd16b410fefe5cb40e271898f959f1cb0
Matkicail <61425800+Matkicail@users.noreply.github.com>
2021-10-24 11:10:20 +0200

    Create em_centroid.py

---
+++ GMM/em_centroid.py
@@ -0,0 +1,70 @@
+import numpy as np
+from numpy.linalg.linalg import _convertarray
+import scipy.stats as stats
+
+
+class EMCCentroid:
+
+    def __init__(self, numK, dataDims):
+        self.numK = numK
+        self.dataDims = dataDims
+        assert np.sum(lams) == 1.0, "initial responsibility should sum to one"
+        self.lams = lams # set of lambdas - M * 1, where M is the number of clusters
+        self.means = means # set of means - M * D, where D is the dimensions of the data
+        self.covs = covs # set of covariances - M * D * D 
+
+    def datapointResponsibilities(self, datapoints):
+        """
+        Given a set of data points - assuming stats can take in a vector and will return a vector.
+        Let there be n data points, of which the dimension d is d > 1
+        """
+        numerators = self.lams * stats.multivariate_normal.pdf(datapoints, self.means, self.covs)
+        denominators = np.sum(numerators)
+        responsibilities = numerators / denominators
+        return responsibilities
+
+    def lambdasUpdate(self, responsibilities):
+        """
+        Given a set of responsibilities for current iteration calculate the new lambda values 
+        responsibilities should a be a matrix of the responsibilities for each point for a given k as the row entries
+        """ 
+        numerators = np.sum(responsibilities, axis=0)
+        denominator = np.sum(numerators)
+        self.lams = numerators/denominator
+        return
+    
+    def meanUpdates(self, datapoints, responsibilities):
+        """
+        Given a set of data points and responsibilities for those datapoints calculate the new means
+        The responsibilities must be a matrix where each row respresents k's responsibility for each data point
+        Let v be the number of data points given which have 
+        """
+        numerator = responsibilities @ datapoints.T # gets a k*n @ n*v => result is k*v column vector
+        denominator = 1 / np.sum(responsibilities, axis = 0) # responsibility along the rows (so should be of length k and a column vector)
+        self.means = numerator * denominator # elementwise multiplication
+        return
+
+    def covariancesUpdate(self, datapoints, responsibilities):
+        """
+        Call this after the mean has been calculated
+        Pass a set of data points and a matrix of responsibilities for those data points
+        The responsibilities must be a matrix where each row respresents k's responsibility for each data point
+        """
+        internal = datapoints - self.means # n*v vector
+        internal = internal @ internal.T # returns an n*n matrix 
+        numerators = np.sum(responsibilities @ internal, axis = 0) # k*n @ n*n => k*n, so need to sum along the columns
+        denominator = np.sum(responsibilities, axis = 0) # getting the total responsibility for K along a row
+        self.covs = numerators / denominator
+
+    def EStep(self, datapoints):
+        return self.datapointResponsibilities(datapoints)
+
+    def MStep(self, responsibilities, datapoints):
+        self.lambdasUpdate(responsibilities)
+        self.meanUpdates(datapoints, responsibilities)
+        self.covariancesUpdate(datapoints, responsibilities)
+
+    def randomParams(self):
+        self.lams = np.ones(self.numK) / self.numK
+        self.means = np.random.random((self.numK,self.dataDims))
+        
